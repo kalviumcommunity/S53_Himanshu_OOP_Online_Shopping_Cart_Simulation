799e4876c68784b2e6fc98d03b951c848401cc67
HiMashuguptA <sg707012@gmail.com>
2024-08-31 14:29:32 +0530

    Usages of classes, Object, This pointer and Array of objects

---
+++ ShoppingCartSimulation.java
@@ -0,0 +1,15 @@
+public class ShoppingCartSimulation {
+    public static void main(String[] args) {
+        Item item1 = new Item("Laptop", 89999, 1);
+        Item item2 = new Item("Smartphone", 49999, 2);
+
+    
+        ShoppingCart cart = new ShoppingCart();
+
+
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        cart.displayCart();
+    }
+}

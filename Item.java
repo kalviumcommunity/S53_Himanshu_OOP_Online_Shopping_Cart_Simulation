890233af816e244e8e74e564dd0ea7cc1ccf8dfd
HiMashuguptA <sg707012@gmail.com>
2024-11-05 15:31:15 +0530

    Added getter and setter

--- Item.java
+++ Item.java
@@ -3,11 +3,37 @@ public class Item {
     private double itemPrice;
     private int itemQuantity;
 
-    public Item(String name, double price, int quantity) {
+    // public Item(String name, double price, int quantity) {
+    //     this.itemName = name;
+    //     this.itemPrice = price;
+    //     this.itemQuantity = quantity;
+    // }
+
+    // Setter
+
+    public void setName(String name){
         this.itemName = name;
+    }
+    public void setPrice(double price){
         this.itemPrice = price;
+    }
+    public void setQuantity(int quantity){
         this.itemQuantity = quantity;
     }
+    
+
+    // getter
+    public void getName(){
+        System.out.println("Name: " + itemName);
+    }
+    public void getPrice(){
+        System.out.println("Price: " + itemPrice); 
+    }
+    public void getQuantity(){
+        System.out.println("Quantity: " + itemQuantity); 
+        
+    }
+
 
     public double getTotalPrice() {
         return itemPrice * itemQuantity;
